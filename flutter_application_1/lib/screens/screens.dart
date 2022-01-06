6f485c5e710cbbccd9e3df24aadab0424e022f02
Anastasiia Zhylina <anastasiiazhylina@gmail.com>
2022-01-06 21:58:27 +0100

    widget container

--- flutter_application_1/lib/screens/screens.dart
+++ flutter_application_1/lib/screens/screens.dart
@@ -4,3 +4,4 @@ export 'package:flutter_application_1/screens/listview1_screen.dart';
 export 'package:flutter_application_1/screens/listview2_screen.dart';
 export 'package:flutter_application_1/screens/home_screen.dart';
 export 'package:flutter_application_1/screens/avatar_screen.dart';
+export 'package:flutter_application_1/screens/animated_screen.dart';
