dbb6f4b40d044445b6815dcb498c8e963bdbb58b
Anastasiia Zhylina <anastasiiazhylina@gmail.com>
2022-01-07 21:38:55 +0100

    add input

--- flutter_application_1/lib/screens/screens.dart
+++ flutter_application_1/lib/screens/screens.dart
@@ -5,3 +5,4 @@ export 'package:flutter_application_1/screens/listview2_screen.dart';
 export 'package:flutter_application_1/screens/home_screen.dart';
 export 'package:flutter_application_1/screens/avatar_screen.dart';
 export 'package:flutter_application_1/screens/animated_screen.dart';
+export 'package:flutter_application_1/screens/input_screen.dart';
