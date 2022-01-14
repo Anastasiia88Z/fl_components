a278915d908f98bc4d6dd548c5891155cc4f970e
Anastasiia Zhylina <anastasiiazhylina@gmail.com>
2022-01-14 23:42:08 +0100

    add change

--- flutter_application_1/lib/screens/slider_screen.dart
+++ flutter_application_1/lib/screens/slider_screen.dart
@@ -16,54 +16,62 @@ class _SliderScreenState extends State<SliderScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Sliders && Checks'),
+          title: const Text('Sliders & Checks'),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Slider.adaptive(
-                  min: 50,
-                  max: 400,
-                  activeColor: AppTheme.primary,
-                  value: _sliderValue,
-                  onChanged: _sliderEnabled
-                      ? (value) {
-                          _sliderValue = value;
-                          setState(() {});
-                        }
-                      : null),
-              CheckboxListTile(
-                  activeColor: AppTheme.primary,
-                  title: const Text('Habilitar Slider'),
-                  value: _sliderEnabled,
-                  onChanged: (value) => setState(() {
-                        _sliderEnabled = value ?? true;
-                      })),
-              SwitchListTile.adaptive(
-                  activeColor: AppTheme.primary,
-                  title: const Text('Habilitar Slider'),
-                  value: _sliderEnabled,
-                  onChanged: (value) => setState(() {
-                        _sliderEnabled = value;
-                      })),
-              //Checkbox(
-              //value: true,
-              //onChanged: (value) {
-              //  _sliderEnabled = value ?? true;
-              //  setState(() {});
-              //}),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Image(
-                    image: const NetworkImage(
-                        'https://st.depositphotos.com/1017994/4251/i/950/depositphotos_42517895-stock-photo-giraffe-isolated-on-white-background.jpg'),
-                    fit: BoxFit.contain,
-                    width: _sliderValue,
-                  ),
+        body: Column(
+          children: [
+            Slider.adaptive(
+                min: 50,
+                max: 400,
+                activeColor: AppTheme.primary,
+                value: _sliderValue,
+                onChanged: _sliderEnabled
+                    ? (value) {
+                        _sliderValue = value;
+                        setState(() {});
+                      }
+                    : null),
+
+            // Checkbox(
+            //   value: _sliderEnabled,
+            //   onChanged: ( value ) {
+            //     _sliderEnabled = value ?? true;
+            //     setState(() {});
+            //   }
+            // ),
+            // Switch(
+            //   value: _sliderEnabled,
+            //   onChanged: ( value ) => setState(() { _sliderEnabled = value; })
+            // ),
+            CheckboxListTile(
+                activeColor: AppTheme.primary,
+                title: const Text('Habilitar Slider'),
+                value: _sliderEnabled,
+                onChanged: (value) => setState(() {
+                      _sliderEnabled = value ?? true;
+                    })),
+
+            SwitchListTile.adaptive(
+                activeColor: AppTheme.primary,
+                title: const Text('Habilitar Slider'),
+                value: _sliderEnabled,
+                onChanged: (value) => setState(() {
+                      _sliderEnabled = value;
+                    })),
+
+            const AboutListTile(),
+
+            Expanded(
+              child: SingleChildScrollView(
+                child: Image(
+                  image: const NetworkImage(
+                      'https://st.depositphotos.com/1017994/4251/i/950/depositphotos_42517895-stock-photo-giraffe-isolated-on-white-background.jpg'),
+                  fit: BoxFit.contain,
+                  width: _sliderValue,
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ));
   }
 }
